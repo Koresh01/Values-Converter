fc2017639f751f53edef66421c0180088c1c446f
Koresh01 <kmatveev2004@gmail.com>
2025-10-08 19:42:46 +0300

    Вынес элемент списка величин измерения как префаб.

---
+++ Assets/Scripts/Value Unit UI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// Представляет один UI-элемент списка конвертера величин.
+/// Отвечает за отображение названия единицы измерения и ввод её значения пользователем.
+/// </summary>
+public class ValueUnitUI : MonoBehaviour
+{
+    [Header("UI Компоненты:")]
+    public TMP_Text label;
+    public TMP_InputField inputField;
+
+    public void Initialize(string name, string formula)
+    {
+        label.text = name;
+        inputField.text = "";
+    }
+}
